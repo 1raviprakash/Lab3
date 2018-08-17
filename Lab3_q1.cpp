23132947670060b976ce5b3bba031241a1caeb8e
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-17 06:18:21 +0000

    Add files via upload

---
+++ Lab3_q1.cpp
@@ -0,0 +1,21 @@
+//library
+#include<iostream>
+using namespace std;
+//write the main function
+int main(){
+// declaring variable
+int A=5;
+char B= 'S';
+float C=5.67;
+double D=5.45632;
+bool E=1;
+// Print the varaibles having different data types
+cout << " A sample integer is " << A << " and the size of integer is " << sizeof(A) << endl;
+cout << " A sample character is " << B << " and the size of character is " << sizeof(B) << endl;
+cout << " A sample float is " << C << " and the size of float is " << sizeof(C) << endl;
+cout << " A sample double is " << D << " and the size of double is " << sizeof(D) << endl;
+cout << " A sample boolen is" << E << " and the size of double is " << sizeof(E) <<endl;
+return 0;
+
+}
+
