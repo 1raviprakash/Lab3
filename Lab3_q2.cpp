10f6965d6134bf6469a57699633026215e0896bf
1raviprakash <42237336+1raviprakash@users.noreply.github.com>
2018-08-17 05:34:16 +0000

    Add files via upload

---
+++ Lab3_q2.cpp
@@ -0,0 +1,23 @@
+//include library
+#include <iostream>
+using namespace std;
+
+//main function
+int main()
+{
+int a,b,sum,substract,product,division,remainder ; //declaration
+a = 7;
+b = 2;
+sum = a + b; // adds 2 numbers
+substract = a - b; //givesthe difference
+product= a * b; //gives product
+division= a / b; //divides the numbers
+remainder= a % b;// remainder of division
+cout << "numbers are"<< a << b << endl;
+cout << "sum is" << sum << endl;
+cout << "difference" << substract << endl;
+cout << "product is" << product << endl;
+cout << "quotient is" << division << endl;
+cout << "remainder is" << remainder << endl;
+return 0;
+}
